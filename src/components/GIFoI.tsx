8101d289de51062d7ad2861ebef5da77f513d4ce
William Bradshaw <will.bradshaw50@gmail.com>
2023-03-30 12:43:53 +1100

    english io

--- src/components/GIFoI.tsx
+++ src/components/GIFoI.tsx
@@ -2,7 +2,7 @@ import "solid-js";
 
 export default function GIFoI() {
   return (
-    <div class="text-gray-600 mb-5">
+    <div class="text-gray-600 mb-3">
       <div>
         <span class="font-bold">Global Issue: </span>
         <span>
@@ -13,6 +13,10 @@ export default function GIFoI() {
       <div>
         <span class="font-bold">Field of inquiry: </span>
         <span>Politics, Power and Justice</span>
+      </div>
+      <div>
+        <span class="font-bold">Personal code: </span>
+        <span>kqb869</span>
       </div>
     </div>
   );
